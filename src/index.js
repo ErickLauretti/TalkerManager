4bfd9a59f68eb0b7618c78e25a10af8bd2765ab6
Erick Lauretti <erick.nighyt@hotmail.com>
2023-06-06 17:18:11 -0300

    FIX: req 11

--- src/index.js
+++ src/index.js
@@ -14,7 +14,6 @@ const {
   validateRate, 
   validateRateIsDecimal,
   validateRateByBody,
-  validateRateIsDecimalByBody,
   validateRateIsDecimalByBodys,
   } = require('./middlewares/middlewares');
 
